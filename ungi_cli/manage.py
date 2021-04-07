ff1c2a7f82d5bc4077b7c558ffe7e033e26f2f89
nsaspy <nsaspy@airmail.cc>
2021-04-07 17:57:15 -0400

    added bulk discord and add modes

--- ungi_cli/manage.py
+++ ungi_cli/manage.py
@@ -8,6 +8,7 @@ from  utils.Sqlite3_Utils import (
     list_ops,
     add_subreddit,
     get_op_id,
+    add_discord,
 )
 from utils.Config import config
 from os import environ
@@ -83,7 +84,7 @@ class OperationsManager(cmd2.Cmd):
         reddit_list: List[List[Any]] = list()
         subreddits = []
         if args.name:
-            op_id = get_op_id(self.database_path, args.name)
+            op_id = get_op_id(self.database_path, args.name)[0]
         else:
             op_id = int(args.id)
         sub_id = 1
@@ -103,6 +104,61 @@ class OperationsManager(cmd2.Cmd):
         else:
             print("Canceled")
 
+    discord_bulk = argparse.ArgumentParser()
+    discord_bulk.add_argument("-i", "--input", help="file containing discord id's")
+    discord_bulk.add_argument("-id", help="operation id")
+    discord_bulk.add_argument("-n", "--name", help="operation_name")
+    @cmd2.with_argparser(discord_bulk)
+    def do_discord_bulk(self, args):
+        columns: List[Column] = list()
+        columns.append(Column("ID", width=12))
+        columns.append(Column("Server ID", width=30,
+                              header_horiz_align=HorizontalAlignment.CENTER, data_horiz_align=HorizontalAlignment.CENTER))
+        reddit_list: List[List[Any]] = list()
+        subreddits = []
+        if args.name:
+            op_id = get_op_id(self.database_path, args.name)[0]
+        else:
+            op_id = int(args.id)
+        sub_id = 1
+        with open(args.input, "r") as reddit_file:
+            for line in reddit_file:
+                reddit_list.append([sub_id, line])
+                subreddits.append(line)
+                sub_id += 1
+
+        bt = BorderedTable(columns)
+        table = bt.generate_table(reddit_list)
+        ansi_print(table)
+        choice = input("Is this ok? (y/n) ")
+        if choice == "y":
+            for sub in subreddits:
+                add_discord(self.database_path, sub, int(op_id[0]))
+        else:
+            print("Canceled")
+
+    add_parser = argparse.ArgumentParser()
+    add_parser.add_argument("-d", help="Discord", action="store_true")
+    add_parser.add_argument("-r", help="Reddit", action="store_true")
+    add_parser.add_argument("-Id", help="Operation id")
+    add_parser.add_argument("-i", help="input")
+    @cmd2.with_argparser(add_parser)
+    def do_add(self, args):
+        if args.d:
+            print(f"adding {args.i}")
+            choice = input("is this ok? (y/n): ")
+            if choice == "y":
+                add_discord(self.database_path, args.i, args.Id)
+            else:
+                print("Canceled")
+
+        if args.r:
+            print(f"adding {args.i}")
+            choice = input("is this ok? (y/n): ")
+            if choice == "y":
+                add_subreddit(self.database_path, args.i, args.Id)
+            else:
+                print("Canceled")
 if __name__ == '__main__':
     import sys
     ops = OperationsManager()
