71fd7924b2be8bd32b1e77efb25bcf516cf76840
nsaspy <nsaspy@airmail.cc>
2021-04-14 15:02:10 -0400

    reworked sql backend

--- loot.sql
+++ loot.sql
@@ -8,64 +8,51 @@ CREATE TABLE "discord" (
 	"discord_id"	INTEGER NOT NULL,
 	"server_id"	INTEGER NOT NULL,
 	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE,
 	PRIMARY KEY("discord_id")
 );
 CREATE TABLE "reddit" (
 	"reddit_id"	INTEGER NOT NULL,
 	"subreddit"	TEXT NOT NULL UNIQUE,
 	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE,
 	PRIMARY KEY("reddit_id")
 );
-CREATE TABLE "discord_users" (
+CREATE TABLE "users" (
 	"user_id"	INTEGER NOT NULL,
-	"usertag"	TEXT NOT NULL UNIQUE,
+	"username"	TEXT NOT NULL,
+	"website"	TEXT NOT NULL,
+	"source"	TEXT,
+	"hash_id"	TEXT,
 	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE,
 	PRIMARY KEY("user_id")
-);
-CREATE TABLE "reddit_users" (
-	"user_id"	INTEGER NOT NULL,
-	"username"	TEXT NOT NULL UNIQUE,
-	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("user_id")
-);
-CREATE TABLE "emails" (
-	"email_id"	INTEGER NOT NULL,
-	"email"	TEXT NOT NULL,
-	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("email_id","email")
-);
-CREATE TABLE "ip" (
-	"ip_id"	INTEGER NOT NULL,
-	"ip"	TEXT NOT NULL UNIQUE,
-	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("ip_id")
-);
-CREATE TABLE "phone_numbers" (
-	"phone_id"	INTEGER NOT NULL,
-	"phone"	TEXT,
-	"operation_id"	INTEGER,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("phone_id")
 );
 CREATE TABLE "watch_list" (
 	"watch_id"	INTEGER NOT NULL,
 	"word:"	TEXT NOT NULL UNIQUE,
 	"operation_id"	INTEGER NOT NULL,
-	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE,
 	PRIMARY KEY("watch_id")
 );
-CREATE TABLE "watch_output" (
-	"output_id"	INTEGER NOT NULL,
-	"output"	TEXT NOT NULL,
-	"date"	INTEGER NOT NULL,
-	"watch_id"	INTEGER NOT NULL,
-	FOREIGN KEY("watch_id") REFERENCES "watch_list"("watch_id") ON UPDATE CASCADE ON DELETE CASCADE,
-	PRIMARY KEY("output_id")
+CREATE TABLE "messages" (
+	"message_id"	INTEGER NOT NULL,
+	"message"	TEXT NOT NULL,
+	"date"	TEXT NOT NULL,
+	"watch_id"	INTEGER,
+	"hash_id"	TEXT UNIQUE
+	FOREIGN KEY("watch_id") REFERENCES "watch_list"("watch_id") ON DELETE CASCADE,
+	PRIMARY KEY("message_id"),
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE
 );
-
+CREATE TABLE "loot" (
+	"loot_id"	INTEGER NOT NULL,
+	"data"	TEXT NOT NULL,
+	"source"	TEXT,
+	"type"	TEXT NOT NULL,
+	"date"	TEXT NOT NULL,
+	"hash_id"	INTEGER UNIQUE,
+	"operation_id"	INTEGER,
+	FOREIGN KEY("operation_id") REFERENCES "operations"("operation_id") ON DELETE CASCADE,
+	PRIMARY KEY("loot_id")
+);
